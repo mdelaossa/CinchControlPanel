43238da9b821c7bda01400ba499c5bbe15721deb
Mario de la Ossa <mariodelaossa@gmail.com>
2015-05-02 22:04:11 -0600

    Implemented WebSocketController tests

--- spec/web_socket_controller_spec.rb
+++ spec/web_socket_controller_spec.rb
@@ -1,7 +1,17 @@
 require 'rspec'
 
 RSpec.describe CinchControlPanel::WebSocketController do
-  it 'can list its descendants'
-  it 'can receive an action'
-  it 'returns a WebSocketMessage as result for an action'
+  it 'can list its descendants' do
+    expect(CinchControlPanel::WebSocketController.descendants.size).to be > 1
+  end
+
+  it 'can receive an action' do
+    expect(subject).to respond_to(:action).with(2).arguments
+  end
+
+  it 'returns a WebSocketMessage as result for an action' do
+    expect(subject).to receive(:test)
+    res = subject.action('test', 'testing')
+    expect(res).to eq(CinchControlPanel::WebSocketMessage.new('websocket', action: 'test', result: nil).to_s)
+  end
 end
